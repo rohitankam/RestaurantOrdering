4eb8f4b08889b478708e3689447f4f1fd09eb15f
rohitankam <rohitankam101@gmail.com>
2018-03-25 00:43:38 +0530

    sign in designed

--- Valley_Bread_Client/app/src/main/java/com/tetteh1568/valley_bread_client/LoginActivity.java
+++ Valley_Bread_Client/app/src/main/java/com/tetteh1568/valley_bread_client/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -58,6 +59,7 @@ public class LoginActivity extends AppCompatActivity {
                 if (dataSnapshot.hasChild(user_id)){
                     Intent menuIntent = new Intent(LoginActivity.this,MenuActivity.class);
                     startActivity(menuIntent);
+                    Toast.makeText(LoginActivity.this,"Success",Toast.LENGTH_SHORT).show();
                 }
             }
 
